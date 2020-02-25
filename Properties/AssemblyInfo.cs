420db519303190ae0cc1edb3fc68cf4b60f209ce
moqaniyani <m.moqaniyani@gmail.com>
2020-02-25 13:11:06 +0330

    Assembly Title Changed

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MiFaVPN")]
+[assembly: AssemblyTitle("VPNUsersManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiFaVPN")]
+[assembly: AssemblyProduct("VPNUsersManager")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
